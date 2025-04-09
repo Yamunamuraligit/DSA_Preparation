ff8a36c1da5a320e5aeeaac263931cad932af0c2
Yamunamuraligit <yamunumurali04@gmail.com>
2025-04-09 11:54:00 +0530

    190.Reverse bits

--- Reverse_bits.java
+++ Reverse_bits.java
@@ -1,3 +1,20 @@
 // Leetcode -- 190
 // Question -- Reverse Bits
 // Reverse bits of a given 32 bits unsigned integer.
+// Methods:
+// 
+
+
+
+
+
+// Code
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        String binary = String.format("%32s",Integer.toBinaryString(n)).replace(' ','0');
+        String reversed = new StringBuilder(binary).reverse().toString();
+        return (int) Long.parseLong(reversed,2);
+
+    }
+}
