6590e63f712b4ee5b44e5e58e1e3536733fbabd1
Yamunamuraligit <yamunumurali04@gmail.com>
2025-04-10 21:55:21 +0530

    338.Counting bits.

--- Counting bits.java
+++ Counting bits.java
@@ -3,7 +3,15 @@
 // Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 // Explanation:
-
+// Here i/2 gives a value that value is automatically converted into bits. after i%2 gives a remainder.
+// That remainder is add the bits convert number.
+// I attached the things to amke more clear with the helpful chatGpt
+// i	Binary	res[i]
+// 0	0000	0
+// 1	0001	1
+// 2	0010	1
+// 5	0101	res[2] + 1 = 1 + 1 = 2
+// 10	1010	res[5] + 0 = 2 + 0 = 2
 
 // Code
 class Solution {
